8de19d8960be305076e0fbbd5d93237d1f72983f
Dan Vega <danvega@gmail.com>
2024-05-23 12:11:20 -0400

    initial commit

---
+++ src/main/java/dev/danvega/vaadin_sessions/VaadinSessionsApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega.vaadin_sessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinSessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSessionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danvega/vaadin_sessions/VaadinSessionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.vaadin_sessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinSessionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
